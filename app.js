993d74c3a82c7afde636eb76d61c57a855f98d37
dengzhiteng <dengzhiteng@jwx.com.cn>
2021-07-13 15:49:48 +0800

    抽离

--- app.js
+++ app.js
@@ -1,18 +1,29 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
 const nunjucks = require("nunjucks");
 
 // 实现静态资源托管
 app.use("/public", express.static(path.join(__dirname, "/public/")));
+app.use(
+  "/node_modules/",
+  express.static(path.join(__dirname, "./node_modules/"))
+);
 
-nunjucks.configure("views", {
+app.use(express.json()); // application/json 格式的数据 {key: value, key: value...}
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+); // application/x-www-form-urlencoded key=value&key=value...
+
+const env = nunjucks.configure(path.join(__dirname, "./view/"), {
   autoescape: true,
   express: app,
+  watch: true, // 启动模板文件监视，文件改变，重新预编译，建议开发阶段开启此功能
 });
 
 app.get("/", function (req, res, next) {
-  res.status(200).render("index.html");
+  res.render("index.html");
 });
 app.listen(3000, () => {});
